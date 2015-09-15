d15b7e92a42b222c750bd60786b0c86a431d202a
Randall Rushing <randall.rushing@gmail.com>
2015-09-15 01:34:39 -0700

    Bring in Eos utilities as a shared library

---
+++ Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := org.teameos.utils
+
+include $(BUILD_JAVA_LIBRARY)
+
+
